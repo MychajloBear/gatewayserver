4b523a43c4fe9fddb02c752edbaf1b3f8056611e
Michajlo <vedmidmetal@ukr.net>
2024-02-13 20:48:19 +0100

    init

---
+++ section9my/gatewayserver/src/main/java/com/eazybytes/gatewayserver/GatewayserverApplication.java
@@ -0,0 +1,15 @@
+package com.eazybytes.gatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDateTime;
+
+@SpringBootApplication
+public class GatewayserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayserverApplication.class, args);
+	}
+}
